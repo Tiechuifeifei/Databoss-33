c6c5975f958ac7e5e7c02a67b9697182adaaf92c
Leo <yhzybzl@gmail.com>
2025-11-30 02:38:17 +0000

    Update bid_functions.php

--- bid_functions.php
+++ bid_functions.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'utilities.php';
 //***********************************************************************************************************************
-//bid_functions.php:
+//bids_functions.php:
 //***********************************************************************************************************************
 // 5 Core Functions:
 //
@@ -20,7 +20,7 @@ require_once 'utilities.php';
 // 5. Place Bid
 //    Allows a buyer to place a bid on an auction item, but with multiple validation checks.
 //***********************************************************************************************************************
-// 6 Validations:
+// 7 Validations:
 //
 // 1. Auction existence check:
 //    Users cannot bid if the auction does not exist.
@@ -32,13 +32,16 @@ require_once 'utilities.php';
 //    Users cannot place a bid on cancelled, scheduled, or already-ended auctions.
 //
 // 4. Minimum bid requirement (no existing bids):
-//    The first bid must be £5 higher than the start price set by the seller.
+//    The first bid must not be less than the start price set by the seller.
 //
 // 5. Minimum bid requirement (with existing bids):
 //    Any new bid must be at least £5.00 higher than the current highest bid.
 //
 // 6. Basic input validation:
 //    Users cannot place a bid that is less than or equal to £0.00 (will also check the non-numeric values too)
+//
+// 7. Basic anti-spam rule:
+//    The current highest bidder cannot place another bid on the same auction.
 //***********************************************************************************************************************
 
     
@@ -47,34 +50,40 @@ require_once 'utilities.php';
 
 function getHighestBidForAuction($auctionId)
 {
-    $db = get_db_connection();
+//build the connection with database
+$db = get_db_connection();
 
-    $sql = "
-        SELECT 
-            b.bidId,
-            b.bidPrice,
-            b.buyerId,
-            u.userName,
-            u.userEmail
-        FROM bids AS b
-        JOIN users AS u ON b.buyerId = u.userId
-        WHERE b.auctionId = ?
-        ORDER BY b.bidPrice DESC, b.bidTime ASC
-        LIMIT 1
-    ";
+$sql = "
+  SELECT
+  b.bidId,
+  b.auctionId,
+  b.bidPrice,
+  b.bidTime,
+  u.userName as buyerName
 
-    $stmt = $db->prepare($sql);
-    $stmt->bind_param("i", $auctionId);
-    $stmt->execute();
+  FROM bids AS b
+  JOIN users AS u on b.buyerId = u.userId
 
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
+  WHERE b.auctionId = ?
+  ORDER BY b.bidPrice DESC, b.bidTime ASC
+  LIMIT 1
+";
 
-    $stmt->close();
+    $stmt = $db -> prepare($sql);
+   
+    $stmt -> bind_param("i", $auctionId);
+    $stmt -> execute();
+    $result = $stmt -> get_result();
+    $row = $result -> fetch_assoc();
 
+//not necessary, but better to have it；also,close MUST before the return!!
+    $stmt -> close();
+
+//没有出价的话，就返回 null 值。
     if (!$row) {
         return null;
     }
+    //不空的话，就返回数组。
     return $row;
 }
 
@@ -218,23 +227,24 @@ return $rows;
 // 1) Validate bid price (> 0).
 // 2) Load auction info and check existence / seller / timing / status.
 // 3) Apply bidding rules:
-//    - First bid must be £5 higher than start price.
-//    - New bid must be at least £5.00 higher than current highest bid.
+//    - First bid must be >= start price.
+//    - Current highest bidder cannot bid again.
+//    -New bid must be at least £5.00 higher than current highest bid.
 //    - Seller cannot bid on own auction.
 // 4) Insert the bid if all checks pass and return a result array.
 
 function placeBid($buyerId, $auctionId, $bidPrice)
 {
-//1. Validate bid price>0 before everything
+// 1)Validate bid price>0 before everything
     if ($bidPrice <= 0) { 
         return [
             "success" => false,
-            "message" => "Sorry, your bid price must be higher than £0.00."
+            "message" => "Sorry,your bid amount must be higher than £0.00."
         ];
     }
     $db = get_db_connection();
 
-//2. Check auction's status, like:existence/start price/status etc.
+// 2)Check auction's status, like:exsistence/start price/status etc.
 $sqlAuction = "
     SELECT 
         i.sellerId,
@@ -255,44 +265,44 @@ $resultAuction = $stmtAuction -> get_result();
 $auctionRow    = $resultAuction -> fetch_assoc();
 $stmtAuction   -> close();
 
-    //(2.1)Auction must exist, otherwise return reminder. 拍卖必须要存在，否则提示不存在然后返回。
+    // (2.1)Auction must exist, otherwise return reminder. 拍卖必须要存在，否则提示不存在然后返回。
     if (!$auctionRow) {
         return [
             "success" => false,
             "message" => "Sorry, this auction does not exist."
         ];
     }
-        //Check the if there is a startPrice, avoid error. 检查起拍价格是不是存在，避免报错。
+        // Check the if there is a startPrice, avoid error. 检查起拍价格是不是存在，避免报错。
     $startPrice = isset($auctionRow["startPrice"]) 
                   ? (float)$auctionRow["startPrice"] 
                   : 0.0;
-        //SellerId for self-bidding validation later. 拿到seller的ID，为后面身份检查做准备。
+        // SellerId for self-bidding validation later. 拿到seller的ID，为后面身份检查做准备。
     $sellerId         = (int)$auctionRow["sellerId"];
-        //get the time for time-based auction check later 保留开始和结束时间，一会判断拍卖的“未开始”和“已结束”。
+        // get the time for time-based auction check later 保留开始和结束时间，一会判断拍卖的“未开始”和“已结束”。
     $auctionStartTime = $auctionRow["auctionStartTime"];
     $auctionEndTime   = $auctionRow["auctionEndTime"];
-        //Use null if auctionStatus is missing or null. 如果有auctionStatus，就用！如果没有status，那就用null!
-        //"??"是null合并运算符号：if the value on left side not exist or is null, then use the value on right side.
-        //这样写其实等同于if (isset($auctionRow["auctionStatus"])) {$auctionStatus = $auctionRow["auctionStatus"];} else {$auctionStatus = null;}
+        // Use null if auctionStatus is missing or null. 如果有auctionStatus，就用！如果没有status，那就用null!
+        // "??"是null合并运算符号：if the value on left side not exist or is null, then use the value on right side.
+        // 这样写其实等同于if (isset($auctionRow["auctionStatus"])) {$auctionStatus = $auctionRow["auctionStatus"];} else {$auctionStatus = null;}
     $auctionStatus    = $auctionRow["auctionStatus"] ?? null; 
-        //get the current time for time-validation later.
+        // get the current time for time-validation later.
     $nowTimestamp     = time();
 
-    //(2.2)Seller cannot bid on his/her own auction, return a reminder禁止卖家给自己的拍卖出价
+    // (2.2) Seller cannot bid on his/her own auction, return a reminder禁止卖家给自己的拍卖出价
     if ((int)$buyerId === $sellerId) {
         return [
             "success" => false,
             "message" => "Sorry, you cannot place a bid on your own auction."
         ];
     }
-    //(2.3)if auction has been cancelled, return reminder 如果拍卖已被手动取消，禁止任何人再出价
+    // (2.3) if auction has been cancelled, return reminder 如果拍卖已被手动取消，禁止任何人再出价
     if ($auctionStatus === 'cancelled') {
         return [
             "success" => false,
             "message" => "Sorry, this auction is not available for bidding anymore."
         ];
     }
-    //(2.4)cannot bid if auction not started yet then return reminder拍卖如果显示的schedule，也不能出价
+    // (2.4) cannot bid if auction not started yet then return reminder拍卖如果显示的schedule，也不能出价
     if (!empty($auctionStartTime)) {
         $startTimestamp = strtotime($auctionStartTime);
         if ($nowTimestamp < $startTimestamp) {
@@ -302,7 +312,7 @@ $stmtAuction   -> close();
             ];
         }
     }
-    //(2.5)Auction already ended, return reminder. 拍卖已结束，不允许再出价
+    // (2.5) Auction already ended, return reminder. 拍卖已结束，不允许再出价
     if (!empty($auctionEndTime)) {
         $endTimestamp = strtotime($auctionEndTime);
         if ($nowTimestamp >= $endTimestamp) {
@@ -312,25 +322,29 @@ $stmtAuction   -> close();
             ];
         }
     }
-//3.Get the Current highest bid for bid validations later. 
-$currentHighest = getHighestBidForAuction($auctionId);
-
-  //(3.1) if no bids, the first bid must be £5 higher than starting price.(bid rule is not just > 0)
+// 3) Get the Current highest bid for bid validations later. 
+    $currentHighest = getHighestBidForAuction($auctionId);
+    // (3.1) First time bid must higher than the start price & check start price>0 btw, otherwise a reminder.首次出价必须高于卖家设置的起拍价
 if ($currentHighest === null) {
-    $minFirstBid = $startPrice + 5.00;  // 起拍价 + £5
-    if ($bidPrice < $minFirstBid) {
+    if ($startPrice > 0 && $bidPrice < $startPrice) {
         return [
             "success" => false,
-            "message" => "Sorry, you are placing the first bid for this auction, it must be at least £5 higher than the starting price."
+            "message" => "Sorry, you are placing the first bid for this auction, it must be at least the start price (£" . number_format($startPrice, 2) . ")."
         ];
     }
 }
-  //if not 3.1,can compare new bid with CHB
-if ($currentHighest !== null) {
+// if not 3.1,can compare new bid with CHB
+    if ($currentHighest !== null) {
     $currentHighestPrice = (float)$currentHighest["bidPrice"];
     $currentHighestBuyer = (int)$currentHighest["buyerId"];
-
-  //(3.2)Minimum increase £5 rule for bid otherwise meaningless最小的加价至少要5镑
+// (3.2) Current highest biddER cannot bid again, otherwise return reminder. 当前最高出价者不能再出价
+    if ($currentHighestBuyer === (int)$buyerId) {
+        return [
+            "success" => false,
+            "message" => "Sorry, you are already the highest bidder for this auction, and cannot place another bid on the same auction."
+        ];
+    }
+    // (3.3) Minimum increase £5 rule for bid otherwise meaningless最小的加价至少要5镑
     $minIncrease = 5.00;  // can be a difference number
     if ($bidPrice < $currentHighestPrice + $minIncrease) {
         return [
@@ -343,6 +357,7 @@ if ($currentHighest !== null) {
     $sql = "
     INSERT INTO bids (auctionId, buyerId, bidPrice, bidTime)
     VALUES (?, ?, ?, NOW())";
+
 
     $stmt = $db -> prepare($sql);
 // "iid" is an arguments list, representing VALUE(?, ?, ?)"
